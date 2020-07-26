d2de84cde30e10543a996c66570695ecfa8681ac
hcarreras <haricarreras@gmail.com>
2020-07-26 15:16:51 +0200

    Add Vehicles Notifier spec
    We want to receive a bunch of vehicles from the scrappers. The notifier (name likely to change) will check which ones are already in the database. In the end, it will send out a report notifying if there are new listed vehicles.

---
+++ spec/services/vehicle_notifier_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+describe VehicleNotifier, type: :service do
+  context "given a bunch of initialized vehicles" do
+    context "when it contains new vehicle" do
+      it "saves the new vehicles" do
+        existing_vehicle_attributes = {
+          title: "Cactus C4",
+          location: "Copenhagen",
+          web_source: "bilbasen"
+        }
+        persisted_vehicle = create(:vehicle, existing_vehicle_attributes)
+        existing_vehicle = build(:vehicle, existing_vehicle_attributes)
+        new_vehicle = build(:vehicle,
+          title: "Cactus C4",
+          location: "Copenhagen",
+          web_source: "bilbasen"),
+        coming_vehicles = [new_vehicle, existing_vehicle]
+
+        service = VehicleNotifier.new(vehicles: coming_vehicles)
+
+        expect(service.execute).to change{ Vehicle.count }.by(1)
+      end
+
+      it "sends a notification"
+    end
+
+    context "when the vehicles already exist" do
+      it "does not save them"
+      it "does not send a notification"
+    end
+  end
+end
