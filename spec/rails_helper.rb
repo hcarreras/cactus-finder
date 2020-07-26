e666ffc6b89a845742732896186cf5600f18e01f
hcarreras <haricarreras@gmail.com>
2020-07-26 15:38:38 +0200

    Add Shoulda matchers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,6 +36,13 @@ RSpec.configure do |config|
 
   config.include FactoryBot::Syntax::Methods
 
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
