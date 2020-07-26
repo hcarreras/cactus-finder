7eab092a3ceb1485c0abdc9f360adb46fa046793
hcarreras <haricarreras@gmail.com>
2020-07-26 15:42:42 +0200

    Refactor vehicles notifier spec
    Using let syntax as the following spec has the same setup

--- spec/services/vehicle_notifier_spec.rb
+++ spec/services/vehicle_notifier_spec.rb
@@ -3,21 +3,24 @@ require 'rails_helper'
 describe VehicleNotifier, type: :service do
   context "given a bunch of initialized vehicles" do
     context "when it contains new vehicle" do
-      it "saves the new vehicles" do
-        existing_vehicle_attributes = {
+      let(:existing_vehicle_attributes) do
+        {
           title: "Cactus C4",
           location: "Copenhagen",
           web_source: "bilbasen"
         }
-        persisted_vehicle = create(:vehicle, **existing_vehicle_attributes)
-        existing_vehicle = build(:vehicle, **existing_vehicle_attributes)
-        new_vehicle = build(:vehicle,
+      end
+      let!(:persisted_vehicle) { create(:vehicle, **existing_vehicle_attributes) }
+      let!(:existing_vehicle) { build(:vehicle, **existing_vehicle_attributes) }
+      let!(:new_vehicle) do
+        build(:vehicle,
           title: "New Cactus C4",
           location: "Aarhus",
           web_source: "bilbasen")
+      end
+      let(:coming_vehicles){ [new_vehicle, existing_vehicle] }
 
-        coming_vehicles = [new_vehicle, existing_vehicle]
-
+      it "saves the new vehicles" do
         service = VehicleNotifier.new(vehicles: coming_vehicles)
 
         expect{ service.execute }.to change{ Vehicle.count }.by(1)
