6fccbe9c6afc0cb80051d032cecd924e60595f6a
hcarreras <haricarreras@gmail.com>
2020-07-26 21:48:22 +0200

    Do not precompile assets
    The app does not have any assets.

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module CactusFinder
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.assets.initialize_on_precompile = false
+    config.assets.enabled = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
